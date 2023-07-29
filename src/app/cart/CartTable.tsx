927f8bb5b3391f566cc9f5d0773818c78c5e5578
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-07-29 15:46:27 +0545

    Working on Delete Cart Routes

--- src/app/cart/CartTable.tsx
+++ src/app/cart/CartTable.tsx
@@ -1,6 +1,7 @@
 "use client"
 import Image from "next/image";
-import { useAppSelector } from "../store";
+import { useAppDispatch, useAppSelector } from "../store";
+import { deleteCart } from "./Features/cartSlice";
 
 export function getCartData() {
     const { cart } = useAppSelector((state) => state.cart);
@@ -10,12 +11,17 @@ export function getCartData() {
 export default function CartTable() {
 
     const cart = getCartData();
+    const dispatch = useAppDispatch();
+
+    const deletCartAction = (id: string) => {
+        dispatch(deleteCart(id));
+    }
 
     return (
-        <table className="w-100" style={{borderSpacing:"0px 10px",borderCollapse:"separate"}}>
+        <table className="w-100" style={{ borderSpacing: "0px 10px", borderCollapse: "separate" }}>
             <thead>
                 <tr>
-                    <th style={{ fontWeight: "700",width:"10%" }} className="fs-5 text-dark">S.N</th>
+                    <th style={{ fontWeight: "700", width: "10%" }} className="fs-5 text-dark">S.N</th>
                     <th style={{ width: "50%", fontWeight: "700" }} className="fs-5 text-dark">Stickers</th>
                     <th style={{ fontWeight: "700" }} className="fs-5 text-dark">Price</th>
                     <th style={{ width: "5%" }}></th>
@@ -27,13 +33,15 @@ export default function CartTable() {
                         <tr key={indx}>
                             <td style={{ fontWeight: "500" }} className="fs-6 text-dark">{indx + 1}</td>
                             <td style={{ fontWeight: "500" }} className=" d-flex gap-2 align-items-center">
-                                <Image src={curr.image} alt={curr.name} width={'60'} height={'60'}/>
+                                <Image src={curr.image} alt={curr.name} width={'60'} height={'60'} />
                                 <h2 className="fs-6 text-dark">{curr.name}</h2>
                             </td>
                             <td style={{ fontWeight: "500" }} className="fs-6 text-dark">{curr.price}</td>
-                            <td><span className="material-symbols-outlined">
-                                delete
-                            </span></td>
+                            <td style={{ cursor: "pointer" }} onClick={() => {
+                                deletCartAction(curr.id)
+                            }} ><span className="material-symbols-outlined">
+                                    delete
+                                </span></td>
                         </tr>
                     ))
                 }
--- src/app/cart/Features/cartSlice.ts
+++ src/app/cart/Features/cartSlice.ts
@@ -1,25 +1,39 @@
-import { ProductsType } from '@/app/const/interface';
-import { createSlice,PayloadAction } from '@reduxjs/toolkit';
+import { ProductsType } from "@/app/const/interface";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 type InitialState = {
-    cart:Array<ProductsType> | [];
-}
+  cart: Array<ProductsType> | [];
+};
 
-const initialState:InitialState = {
-    cart: [] 
-}
+const initialState: InitialState = {
+  cart: [],
+};
 const CartSlice = createSlice({
-    name:"Cart",
-    initialState,
-    reducers:{
-        setCart:(state,action: PayloadAction<Array<ProductsType>>) => {
-            state.cart = action.payload
-        },
-        addCart:(state,action: PayloadAction<ProductsType>) => {
-            state.cart = [...state.cart,action.payload]
+  name: "Cart",
+  initialState,
+  reducers: {
+    setCart: (state, action: PayloadAction<Array<ProductsType>>) => {
+      state.cart = action.payload;
+    },
+    addCart: (state, action: PayloadAction<ProductsType>) => {
+      state.cart = [...state.cart, action.payload];
+    },
+    deleteCart: (state, action: PayloadAction<string>) => {
+      var index = null;
+      var initalArray = state.cart;
+      for (let i = 0; i < state.cart.length; i++) {
+        if (state.cart[i].id == action.payload) {
+          index = i;
+          break;
         }
-    }, 
+      }
+      if (index != null) {
+        initalArray.splice(index, 1);
+        state.cart = initalArray
+      }
+    },
+  },
 });
 
 export default CartSlice.reducer;
-export const {setCart,addCart} = CartSlice.actions; 
+export const { setCart, addCart, deleteCart } = CartSlice.actions;
