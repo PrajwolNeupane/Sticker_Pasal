a48e96dec509a9bb3b70cf90c47de53611f2f874
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-07-31 14:34:49 +0545

    Fixed User Me Route Error

--- src/app/api/user/me/route.ts
+++ src/app/api/user/me/route.ts
@@ -5,18 +5,20 @@ import { getDataFromToken } from "@/helper/getDataFromToken";
 
 export async function GET(request: NextRequest) {
   try {
-   const id =  getDataFromToken(request);
-    var user = await User.findById({ _id:id }).select(
-      ["-password","-__v"]
-    );
+    const id = getDataFromToken(request);
+    if (id) {
+      var user = await User.findById({ _id: id }).select(["-password", "-__v"]);
 
-    const response = NextResponse.json({
-      message: "User Information Rettivied Successfully",
-      success: true,
-      user: user,
-    });
-    
-    return response;
+      const response = NextResponse.json({
+        message: "User Information Rettivied Successfully",
+        success: true,
+        user: user,
+      });
+      return response;
+    } else {
+      const response = NextResponse.json({ message:"No User Login" }, { status: 500 });
+      return response;
+    }
   } catch (e: any) {
     console.log(e);
     return NextResponse.json({ error: e.message }, { status: 500 });
--- src/helper/getDataFromToken.ts
+++ src/helper/getDataFromToken.ts
@@ -4,8 +4,12 @@ import jwt from "jsonwebtoken";
 export function getDataFromToken(request: NextRequest) {
   try {
     const token = request.cookies.get("token")?.value || "";
-    const decodedToken:any = jwt.verify(token,process.env.TOEKN_SECRET!);
-    return decodedToken.id;
+    if (token == "" || token == null) {
+      return null;
+    } else {
+      const decodedToken: any = jwt.verify(token, process.env.TOEKN_SECRET!);
+      return decodedToken.id;
+    }
   } catch (e: any) {
     console.log(e.message);
   }
