bbca9e05413a63b2fcf12e05441c57384ceb468e
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-07-31 14:16:47 +0545

    Updated MongoDB URL

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -1,20 +1,28 @@
 import mongoose from "mongoose";
 
 export async function connect() {
-    try{
-        mongoose.connect(`mongodb+srv://prajwolneupane68:${encodeURIComponent(process.env.DB_PASSWORD!)}@cluster0.zd1cys6.mongodb.net/Main?retryWrites=true&w=majority`);
-
-        const connection = mongoose.connection;
-
-        connection.on('connected',()=>{
-        })
-
-        connection.on('error',(error) => {
-            console.log('MongoDB connected error');
-            console.log(error);
-        })
-
-    }catch(error){
-        console.log(error);
+  try {
+    if (process.env.MONGODB_URI) {
+      mongoose.connect(
+        `${process.env.MONGODB_URI}`
+      );
+    } else {
+      mongoose.connect(
+        `mongodb+srv://prajwolneupane68:${encodeURIComponent(
+          process.env.DB_PASSWORD!
+        )}@cluster0.zd1cys6.mongodb.net/Main?retryWrites=true&w=majority`
+      );
     }
+
+    const connection = mongoose.connection;
+
+    connection.on("connected", () => {});
+
+    connection.on("error", (error) => {
+      console.log("MongoDB connected error");
+      console.log(error);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
